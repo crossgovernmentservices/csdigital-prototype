29d83249300975af95c0ed473be455d9e17c9f74
Adam Shimali <adam.shimali@gmail.com>
2015-11-12 11:02:57 +0000

    flask script needed for prod

--- environment.sh
+++ environment.sh
@@ -3,6 +3,6 @@ export SASS_PATH='.'
 export SECRET_KEY='local-dev-not-secret'
 export SECURITY_PASSWORD_HASH='bcrypt'
 export SECURITY_POST_LOGIN_VIEW='/profile'
-export MONGODB_DB='xgs_performance_reviews'
+export MONGODB_DATABASE='xgs_performance_reviews'
 export MONGODB_HOST='localhost'
 export MONGODB_PORT=27017
