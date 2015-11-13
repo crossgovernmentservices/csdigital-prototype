383ad1da3446c1b0b6345ce3d0c256c33f99f947
Adam Shimali <adam.shimali@gmail.com>
2015-11-13 15:36:44 +0000

    Not sure if this is a good idea, but starting the feedback template page with a content editable section.

--- application/static/javascripts/app.js
+++ application/static/javascripts/app.js
@@ -6,9 +6,24 @@ var toggleObjective = function(event){
     event.preventDefault();
     details.toggle();
     toggle.toggle();
+};
 
+var templateEditable = function(event){
+    event.preventDefault();
+    var editLink = event.currentTarget
+        editableSection = $(editLink).next();
+
+    if( $(editLink).attr('contenteditable') == 'true'){
+        $(editableSection).attr('contenteditable', 'false');
+        $(editableSection).blur();
+    } else {
+        $(editableSection).attr('contenteditable', 'true');
+        $(editableSection).focus();
+    }
+    $('.edit-controls a').toggle();
 };
 
 $(document).ready(function(){
     $('.objective-header').click(toggleObjective);
+    $('.edit-controls').click(templateEditable);
 });
