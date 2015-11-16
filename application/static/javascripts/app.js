6ed3e73304ff9d5a97b82d493c6ac797457fe352
Adam Shimali <adam.shimali@gmail.com>
2015-11-16 11:59:59 +0000

    fix some copy. Add js for enable/disable textarea

--- application/static/javascripts/app.js
+++ application/static/javascripts/app.js
@@ -1,30 +1,30 @@
 var toggleObjective = function(event){
-    console.log('here');
-    var clicked = event.currentTarget,
-        details = $(clicked).next(),
-        toggle = $(clicked).find('.toggle a');
-    event.preventDefault();
-    details.toggle();
-    toggle.toggle();
+  console.log('here');
+  var clicked = event.currentTarget,
+    details = $(clicked).next(),
+      toggle = $(clicked).find('.toggle a');
+  event.preventDefault();
+  details.toggle();
+  toggle.toggle();
 };
 
 var templateEditable = function(event){
-    event.preventDefault();
-    var editLink = event.currentTarget
-        editableContainer = $(editLink).next(),
-            editableSection = $(editableContainer).find('textarea');
+  event.preventDefault();
+  var editLink = event.currentTarget,
+    editableContainer = $(editLink).next(),
+      editableSection = $(editableContainer).find('textarea');
 
-    if( $(editableSection).attr('disabled') ){
-        $(editableSection).removeAttr('disabled');
-        $(editableSection).focus();
-    } else {
-        $(editableSection).attr('disabled', 'true');
-        $(editableSection).blur();
-    }
-    $('.edit-controls a').toggle();
+  if( $(editableSection).attr('disabled') ){
+    $(editableSection).removeAttr('disabled');
+    $(editableSection).focus();
+  } else {
+    $(editableSection).attr('disabled', 'true');
+    $(editableSection).blur();
+  }
+  $('.edit-controls a').toggle();
 };
 
 $(document).ready(function(){
-    $('.objective-header').click(toggleObjective);
-    $('.edit-controls').click(templateEditable);
+  $('.objective-header').click(toggleObjective);
+  $('.edit-controls').click(templateEditable);
 });
