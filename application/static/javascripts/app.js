4997f798105ef513839be262e1b258f5daf57a0e
Adam Shimali <adam.shimali@gmail.com>
2015-11-17 12:29:33 +0000

    start implementation of user picker for feedback

--- application/static/javascripts/app.js
+++ application/static/javascripts/app.js
@@ -1,4 +1,4 @@
-var toggleObjective = function(event){
+var toggleObjective = function(event) {
   console.log('here');
   var clicked = event.currentTarget,
     details = $(clicked).next(),
@@ -8,7 +8,7 @@ var toggleObjective = function(event){
   toggle.toggle();
 };
 
-var templateEditable = function(event){
+var templateEditable = function(event) {
   event.preventDefault();
   var editLink = event.currentTarget,
     editableContainer = $(editLink).next(),
@@ -25,65 +25,38 @@ var templateEditable = function(event){
 };
 
 
-var userLookup = function(event){
+var userLookup = function(event) {
   event.preventDefault();
-
   var input = $("input[name='q']"),
       searchValue = input.val().trim();
-
-  if(searchValue.length < 3){
-    console.log('do nothing');
-    $('#company-selector').hide();
-    $('#companies').empty();
-    return;
-  }
-
-  $('#user-selector').hide();
-  $('#users').empty();
-  $('.message').empty();
-
   $.ajax({
     type: 'GET',
-    url: '/user-search?q='+searchValue,
+    url: '/users.json?q='+searchValue,
     contentType: 'application/json',
     success: function(data) {
-        renderUsers(data);
-        $('#user-selector').show();
-        $('.user-link').click(stuffAddressInPage);
+        renderUsers(data.users);
     },
     error: function(xhr, options, error) {
       console.log(error);
-      $('#user-selector').empty();
       $('.message').text('No results');
     }
   });
 };
 
-var stuffUsersIntoPage = function(event){
-  event.preventDefault();
-  $('#selected-user').text('');
-  var template = $.templates("#selected-user-template"),
-      data = $(event.currentTarget).data(),
-      html = template.render({
-          'user_email': event.currentTarget.text,
-      });
-  $('#selected-user').append(html);
-  $('#user-selector').hide();
-  $('#users').empty();
-};
 
-var renderUsers = function(addresses) {
-  $.each(addresses, function(index, user) {
+var renderUsers = function(users) {
+  $.each(users, function(index, user) {
     var template = $.templates("#user-template"),
       html = template.render({
-        'user': user.email
+        'userEmail': user.email
     });
     $('#users').append(html);
   });
 };
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.objective-header').click(toggleObjective);
   $('.edit-controls').click(templateEditable);
+  $('#user-lookup').click(userLookup);
 });
