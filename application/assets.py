78ea607d8c7c685dee1bfe6afb35b8bbebf45c11
Colm Britton <colmjude@gmail.com>
2015-12-11 11:39:41 +0000

    update to use main syntax scss over sass

--- application/assets.py
+++ application/assets.py
@@ -3,16 +3,16 @@ import os
 from flask_assets import Bundle, Environment
 from webassets.filter import get_filter
 
-sass = get_filter('sass', as_output=True)
+scss = get_filter('scss', as_output=True)
 
-sass.load_paths = [os.path.join(os.path.dirname(__file__), 'static/sass'),
+scss.load_paths = [os.path.join(os.path.dirname(__file__), 'static/sass'),
                     os.path.join(os.path.dirname(__file__), 'static/govuk_frontend_toolkit/stylesheets'),
                     os.path.join(os.path.dirname(__file__), 'static/govuk_elements/public/sass/elements')]
 
 
 css_govuk_elements = Bundle(
     'sass/govuk_elements.scss',
-    filters=sass,
+    filters=scss,
     output='stylesheets/govuk_elements.css',
     depends=['/static/govuk_elements/public/sass/**/*.scss',
                 '/static/govuk_frontend_toolkit/stylesheets/**/*.scss']
@@ -20,7 +20,7 @@ css_govuk_elements = Bundle(
 
 css_main = Bundle(
     'sass/main.scss',
-    filters=sass,
+    filters=scss,
     output='stylesheets/main.css',
     depends=['/static/sass/main/**/*.scss','/static/govuk_frontend_toolkit/stylesheets/**/*.scss']
 )
