b32a2b177761a284447c51d2fb36352d24ebef11
Kerin Cosford <cosford@gmail.com>
2015-12-23 15:56:38 +0000

    Event queue now uses a topic exchange instead of a simple direct queue

--- application/queues.py
+++ application/queues.py
@@ -1,33 +1,39 @@
 import socket
 from time import time
 
-from kombu import Connection
+from kombu import Connection, Exchange
+from kombu.pools import producers
 
 from flask import current_app
 
 
 class EventQueue(object):
-    def __init__(self, broker_uri, queue_name='event_queue', serializer='json',
-                 compression=None):
+    def __init__(self, broker_uri, serializer='json', compression=None):
+        self.exchange = Exchange('events', 'topic', durable=True)
         self.broker_uri = broker_uri
-        self.queue_name = queue_name
         self.serializer = serializer
         self.compression = compression
 
     def send(self, entity, action, context={}):
         with Connection(self.broker_uri) as conn:
-            try:
-                queue = conn.SimpleQueue(self.queue_name,
-                                         exchange_opts={'type': 'topic'})
-            except conn.connection_errors as err:
-                current_app.logger.warning(
-                    'Broker connection failed with: %s', err)
-                return
-
-            queue.put({
+            payload = {
                 'entity': entity,
                 'action': action,
                 'context': context,
                 'hostname': socket.gethostname(),
                 'timestamp': time()
-            }, serializer=self.serializer, compression=self.compression)
+            }
+
+            try:
+                with producers[conn].acquire(block=True) as producer:
+                    producer.publish(payload, serializer=self.serializer,
+                                     compression=self.compression,
+                                     exchange=self.exchange,
+                                     declare=[self.exchange])
+
+                    current_app.logger.info('Event sent: entity=%s, action=%s',
+                                            entity, action)
+            except conn.connection_errors as err:
+                current_app.logger.warning(
+                    'Broker connection failed with: %s', err)
+                return
