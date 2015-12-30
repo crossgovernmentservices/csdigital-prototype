c24cd49931ef299c36eff23e1c0477fb31080089
Colm Britton <colmjude@gmail.com>
2015-12-30 17:13:37 +0000

    add the bare viewer page skeleton

--- application/journeys/views.py
+++ application/journeys/views.py
@@ -8,3 +8,7 @@ journeys = Blueprint('journeys', __name__, template_folder='templates')
 @journeys.route('/journeys')
 def journeys_home():
     return render_template('journeys/home.html')
+
+@journeys.route('/journeys/viewer')
+def journeys_viewer():
+    return render_template('journeys/viewer.html')
---
+++ application/static/javascripts/journeys.js
@@ -0,0 +1,5 @@
+(function($, window) {
+
+  $(function() {});
+
+}).call(this, jQuery, window);
