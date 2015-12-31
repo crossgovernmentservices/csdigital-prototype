a638801698471a1b2dbbedfd9a3a3ae7e7c9e1f3
Colm Britton <colmjude@gmail.com>
2015-12-31 14:14:40 +0000

    handle error if datafile does not exist

--- application/journeys/views.py
+++ application/journeys/views.py
@@ -4,6 +4,7 @@ from flask import (
 )
 
 import json
+import os.path
 
 journeys = Blueprint('journeys', __name__, template_folder='templates')
 
@@ -14,6 +15,9 @@ def journeys_home():
 @journeys.route('/journeys/<journey>')
 def journeys_viewer(journey):
     datafile = "application/data/" + journey + ".json"
-    with open( datafile ) as data_file:
-          journeys = json.load(data_file)
-    return render_template('journeys/viewer.html', journeys=journeys)
+    if os.path.isfile( datafile ):
+      with open( datafile ) as data_file:
+            journeys = json.load(data_file)
+      return render_template('journeys/viewer.html', journeys=journeys)
+    else:
+      return render_template("404.html"), 404
