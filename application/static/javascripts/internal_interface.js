6300fde6a9a7f1d1aa69e98388960f135363b692
Colm Britton <colmjude@gmail.com>
2016-01-22 14:22:01 +0000

    only reveal linking controls if required

--- application/static/javascripts/internal_interface.js
+++ application/static/javascripts/internal_interface.js
@@ -8,6 +8,17 @@
       $('.add-staff-member-form').toggle();
       return false;
     });
+
+    var $linkForms = $('.add-link-form');
+    // toggle add another link form
+    $('.add-link-btn').on('click', function() {
+      $(this).toggleClass('active');
+
+      var linkType = $(this).data('link-form');
+      $linkForms.filter( "[data-linking-type='" + linkType + "']" ).toggle();
+      return false;
+    });
+
   });
 
 }).call(this, jQuery, window);
