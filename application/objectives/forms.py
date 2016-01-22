00d30496e893abf209be88da4831a3bf0bbfcd69
Andy Driver <andy@pagezero.net>
2016-01-22 17:35:27 +0000

    Add progress field to objectives

--- application/objectives/forms.py
+++ application/objectives/forms.py
@@ -1,11 +1,10 @@
 import datetime
 
-from flask.ext.login import current_user
 from flask.ext.wtf import Form
 from wtforms.validators import DataRequired, Required
 from wtforms.fields import StringField, TextAreaField
 
-from application.models import Entry, LogEntry
+from application.models import create_log_entry
 from application.utils import a_year_from_now
 
 
@@ -13,28 +12,24 @@ class ObjectiveForm(Form):
     title = StringField('Title', validators=[Required()])
     what = TextAreaField('What is your objective?', validators=[Required()])
     how = TextAreaField('How will you achieve this?', validators=[Required()])
+    progress = TextAreaField('What progress have you made?')
 
     def update(self, objective):
         objective.entry.update(
             title=self.title.data,
             what=self.what.data,
+            progress=self.progress.data,
             how=self.how.data)
-        objective.entry.save()
 
     def create(self):
-        entry = Entry(
+        objective = create_log_entry(
+            'objective',
             title=self.title.data,
             what=self.what.data,
             how=self.how.data,
+            progress=self.progress.data,
             started_on=datetime.datetime.utcnow(),
             due_by=a_year_from_now())
-        entry.save()
-
-        objective = LogEntry(
-            entry_type='objective',
-            owner=current_user._get_current_object(),
-            entry=entry)
-        objective.save()
 
         objective.add_tag('Objective')
 
