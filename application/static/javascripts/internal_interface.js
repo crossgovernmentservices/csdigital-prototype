b5597d374ae8cc17a4b476d53967604b1cc4eccc
Colm Britton <colmjude@gmail.com>
2016-01-22 11:55:45 +0000

    clearer button and form association for adding staff to note

--- application/static/javascripts/internal_interface.js
+++ application/static/javascripts/internal_interface.js
@@ -4,6 +4,7 @@
 
     // toggle add staff member form
     $('.staff-list__add-btn').on('click', function() {
+      $(this).toggleClass('active');
       $('.add-staff-member-form').toggle();
       return false;
     });
