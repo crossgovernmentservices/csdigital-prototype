65ffee016ba4772fefaeaf8d46ac0f69f5b2e097
Andy Driver <andy@pagezero.net>
2016-01-27 14:11:16 +0000

    Simply competency views and split templates

--- application/competency/models.py
+++ application/competency/models.py
@@ -11,6 +11,18 @@ class Level(db.Document):
     description = db.StringField()
     level_id = db.IntField()
 
+    @property
+    def prev(self):
+        if '_prev' not in self and self.level_id > 1:
+            self._prev = Level.objects.get(level_id=self.level_id - 1)
+        return self._prev
+
+    @property
+    def next(self):
+        if '_next' not in self and self.level_id < 6:
+            self._next = Level.objects.get(level_id=self.level_id + 1)
+        return self._next
+
 
 class CompetencyCluster(db.Document):
     name = db.StringField(required=True)
@@ -52,6 +64,11 @@ class Competency(db.Document):
                 return True
 
         return False
+
+    def behaviours(self, level):
+        return Behaviour.objects.filter(
+            level=level,
+            competency=self)
 
 
 class Behaviour(db.Document):
--- application/competency/views.py
+++ application/competency/views.py
@@ -1,6 +1,5 @@
 from flask import (
     Blueprint,
-    abort,
     flash,
     redirect,
     request,
@@ -9,13 +8,13 @@ from flask import (
 from flask.ext.login import current_user
 from flask.ext.security import login_required
 
-from application.competency.forms import behaviours_form, make_link_form
+from application.competency.forms import make_link_form
 from application.competency.models import (
-    Behaviour,
     Competency,
     Level)
 from application.models import LogEntry
 from application.profile.views import update_details_form
+from application.utils import get_or_404
 
 
 competency = Blueprint('competency', __name__, template_folder='templates')
@@ -26,18 +25,15 @@ def competencies():
     return dict(competencies=Competency.objects)
 
 
-def get_competency_or_404(id):
-    try:
-        return Competency.objects.get(id=id)
-
-    except Competency.DoesNotExist:
-        abort(404)
+@competency.context_processor
+def grade_form():
+    return dict(grade_form=update_details_form())
 
 
 @competency.route('/competency/<id>/link', methods=['POST'])
 @login_required
 def link(id):
-    competency = get_competency_or_404(id)
+    competency = get_or_404(Competency, id=id)
     form = make_link_form(objectives=True, notes=True)
 
     if form.validate_on_submit():
@@ -53,7 +49,7 @@ def link(id):
 @competency.route('/competency/<id>/unlink/<link_id>', methods=['GET', 'POST'])
 @login_required
 def unlink(id, link_id):
-    competency = get_competency_or_404(id)
+    competency = get_or_404(Competency, id=id)
 
     if competency.unlink(link_id):
         flash('Removed link')
@@ -65,62 +61,31 @@ def unlink(id, link_id):
 
 
 @competency.route('/competency', methods=['GET', 'POST'])
+def view_all():
+    return render_template('competency/view_all.html')
+
+
 @competency.route('/competency/<id>', methods=['GET', 'POST'])
 @competency.route('/competency/<id>/<level_id>', methods=['GET', 'POST'])
 @login_required
 def view(id=None, level_id=None):
-    competency = None
-    if id:
-        competency = Competency.objects.get(id=id)
-
     level = None
+
     if level_id:
-        level = Level.objects.get(id=level_id)
+        level = get_or_404(Level, id=level_id)
 
-    link_form = make_link_form(objectives=True)
-
-    behaviours = []
+    elif current_user.grade:
+        level = Level.objects.get(description=current_user.grade)
 
     if not current_user.grade:
         form = update_details_form()
 
         if form.validate_on_submit():
-            current_user.grade = form.grade.data
-            current_user.save()
+            current_user.update(grade=form.grade.data)
             flash('Successfully updated profile')
             return redirect(url_for('.view', id=id))
 
-        return render_template(
-            'competency/view-competency.html',
-            competency=competency,
-            level=level,
-            link_form=link_form,
-            form=form)
-
-    if not level:
-        level = Level.objects.get(description=current_user.grade)
-
-    prev_level = None
-    if level.level_id > 1:
-        prev_level = Level.objects.get(level_id=level.level_id - 1)
-
-    next_level = None
-    if level.level_id < 6:
-        next_level = Level.objects.get(level_id=level.level_id + 1)
-
-    form = None
-    if competency:
-        behaviours = Behaviour.objects.filter(
-            level=level,
-            competency=competency)
-        form = behaviours_form(behaviours)
-
     return render_template(
         'competency/view-competency.html',
-        behaviours=behaviours,
-        form=form,
-        link_form=link_form,
         level=level,
-        prev_level=prev_level,
-        next_level=next_level,
-        competency=competency)
+        competency=get_or_404(Competency, id=id))
