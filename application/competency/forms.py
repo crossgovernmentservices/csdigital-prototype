bb1eb380e8364878247cbef443bd64c829a190db
Andy Driver <andy@pagezero.net>
2016-01-27 14:14:34 +0000

    Remove unused BehaviourForm

--- application/competency/forms.py
+++ application/competency/forms.py
@@ -1,24 +1,6 @@
 from flask.ext.login import current_user
 from flask.ext.wtf import Form
-from wtforms.fields import SelectField, SelectMultipleField
-from wtforms.widgets import CheckboxInput, ListWidget
-
-
-class MultiCheckboxField(SelectMultipleField):
-    widget = ListWidget(prefix_label=False)
-    option_widget = CheckboxInput()
-
-
-class BehaviourForm(Form):
-    behaviours = MultiCheckboxField()
-
-
-def behaviours_form(behaviours):
-    form = BehaviourForm()
-    form.behaviours.choices = [
-        (behaviour.id, behaviour.effective)
-        for behaviour in behaviours]
-    return form
+from wtforms.fields import SelectField
 
 
 def make_link_form(**enable):
