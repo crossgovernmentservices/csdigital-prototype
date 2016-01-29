e13359e4ddd9bb5bf6e44e538aeddfda884afb71
Colm Britton <colmjude@gmail.com>
2016-01-29 21:54:06 +0000

    display list of competencies not associated with a user's objectives

--- application/competency/views.py
+++ application/competency/views.py
@@ -63,7 +63,8 @@ def unlink(id, other_id):
 
 @competency.route('/competency', methods=['GET', 'POST'])
 def view_all():
-    return render_template('competency/view_all.html')
+    not_linked = Competency.objects.filter(id__nin=[c.id for c in current_user.competencies])
+    return render_template('competency/view_all.html', comps_not_linked=not_linked)
 
 
 @competency.route('/competency/<id>', methods=['GET', 'POST'])
