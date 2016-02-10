a1fc56579a5e9a47d54fbe99a12bebabd459476d
Andy Driver <andy@pagezero.net>
2016-02-10 14:58:10 +0000

    Add REST JSON API endpoint to fetch behaviours for a competency at a given level

--- application/competency/views.py
+++ application/competency/views.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint,
     flash,
+    jsonify,
     redirect,
     request,
     render_template,
@@ -65,6 +66,17 @@ def unlink(id, other_id):
 def view_all():
     not_linked = Competency.objects.filter(id__nin=[c.id for c in current_user.competencies])
     return render_template('competency/view_all.html', comps_not_linked=not_linked)
+
+
+@competency.route('/competency/<id>/level/<level_id>.json')
+@login_required
+def competency_json(id, level_id):
+    level = get_or_404(Level, id=level_id)
+    competency = get_or_404(Competency, id=id)
+    return jsonify({
+        'competency': competency,
+        'level': level,
+        'behaviours': competency.behaviours(level)})
 
 
 @competency.route('/competency/<id>', methods=['GET', 'POST'])
