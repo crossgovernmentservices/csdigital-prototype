6ebbb3cdbc861c2421a7de88785400d7cec22808
Colm Britton <colmjude@gmail.com>
2016-05-09 09:10:17 +0100

    save user tags to localStorage

--- application/static/javascripts/create-tag.js
+++ application/static/javascripts/create-tag.js
@@ -1,4 +1,29 @@
+// extend Local Storage Object
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+};
+
+Storage.prototype.getObject = function(key) {
+    return this.getItem(key) && JSON.parse(this.getItem(key));
+};
+
 (function($, window) {
+
+  // make it persist across prototype pages ??
+  var updateLocalList = function( tag ) {
+    var tags = localStorage.getObject('tags');
+    var tagObj = {
+      "name": tag,
+      "count": 0
+    };
+
+    if( tags ) {
+      tags.push( tagObj );
+      localStorage.setObject( "tags", tags);
+    } else {
+      localStorage.setObject( "tags", [tagObj]);
+    }
+  };
 
   var addTag = function( tag, $list ) {
     var $tag = $("<li>").text( tag ).addClass("note-tag");
@@ -12,12 +37,20 @@
     $tag_input.on('keydown', function(evt) {
       var $target = $(evt.currentTarget);
       if( evt.keyCode == 13 ) {
-        addTag( $target.val(), $tag_list);
+        var tag = $target.val();
+        addTag( tag, $tag_list);
         evt.stopPropagation();
         $tag_input.val("").focus();
+        updateLocalList( tag );
       }
     })
     .parents('form').on('submit', function() {
+      var tag = $tag_input.val();
+      if( tag.length > 0 ) {
+        addTag( tag, $tag_list);
+        $tag_input.val("").focus();
+        updateLocalList( tag );
+      }
       return false;
     });
   });
