17d786de2c469db2cbe4cd0240330406c0576a71
Colm Britton <colmjude@gmail.com>
2016-06-03 15:49:44 +0100

    fake adding emails form

--- application/static/javascripts/onboarding.js
+++ application/static/javascripts/onboarding.js
@@ -2,7 +2,9 @@
 
   $(function() {
     var $form = $(".objective-tags-form"),
-        $first = $(".tag-entry-group").first();
+        $first = $(".tag-entry-group").first(),
+        $emailForm = $(".additional-email-form"),
+        $emailList = $(".list-emails");
 
     $(".add-tag-btn").on("click", function(evt) {
       var $input = $first.clone(true).find(".tag-entry-field").val("").end();
@@ -15,6 +17,31 @@
       $(evt.currentTarget).parents(".tag-entry-group").remove();
       return false;
     });
+
+    // listen for clicks on btn to add email
+    $emailForm.find("button").on("click", function(evt) {
+      var $input = $emailForm.find("input"),
+          email = $input.val();
+      addEmailToList( email, $emailList );
+      $input.val("");
+      return false;
+    });
+
+    // remove item if link is clicked
+    $emailList.on("click", "a.remove-link", function() {
+      $(this).parents("li").remove();
+      return false;
+    });
+
+    // function to add item to email list
+    var addEmailToList = function( email, $list ) {
+      var $item = $emailList.find("li").last().clone(),
+          $removeLink = $("<span>").append( $("<a>").attr("href", "/").addClass("remove-link").text("Remove") );
+
+      $item.empty().text( email ).append( $removeLink );
+      $list.append( $item );
+    };
+
   });
 
 }).call(this, jQuery, window);
